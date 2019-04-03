8f1f47bd95c4e16893235443419e25c1efac7c20
sporchia <print@blindchaos.net>
2019-04-02 20:22:19 -0400

    clearing up some todos :anchor:
    * merging unnecessary traits into the only class they exist in.

--- src/Graph.php
+++ src/Graph.php
@@ -7,24 +7,17 @@ use PHGraph\Contracts\Directable;
 use PHGraph\Support\EdgeCollection;
 use PHGraph\Support\VertexCollection;
 use PHGraph\Traits\Attributes;
-use PHGraph\Traits\Degreed;
-use PHGraph\Traits\Directed;
-use PHGraph\Traits\Grouped;
+use UnderflowException;
+use UnexpectedValueException;
 
 /**
  * Representation of a Mathematical Graph.
  *
  * @todo consider removal of vertices and edges, how would we best go about this?
- * @todo consider merging Degreed trait into this to simplify overall code.
- * @todo consider merging Directed trait into this to simplify overall code.
- * @todo consider merging Grouped trait into this to simplify overall code.
  */
 class Graph implements Attributable, Directable
 {
     use Attributes;
-    use Degreed;
-    use Directed;
-    use Grouped;
 
     /** @var \PHGraph\Support\VertexCollection */
     protected $vertices;
@@ -35,6 +28,54 @@ class Graph implements Attributable, Directable
     public function __construct()
     {
         $this->vertices = new VertexCollection;
+    }
+
+    /**
+     * Degree: get degree for k-regular-graph (only if each vertex has the same
+     * degree).
+     *
+     * @throws UnderflowException       if graph is empty
+     * @throws UnexpectedValueException if graph is not regular
+     *
+     * @return int
+     */
+    public function getDegree(): int
+    {
+        $degree = $this->vertices->first()->degree();
+
+        foreach ($this->vertices as $vertex) {
+            $i = $vertex->degree();
+
+            if ($i !== $degree) {
+                throw new UnexpectedValueException('Graph is not k-regular (vertex degrees differ)');
+            }
+        }
+
+        return $degree;
+    }
+
+    /**
+     * Degree: get minimum degree of vertices.
+     *
+     * @return int
+     */
+    public function getDegreeMin(): int
+    {
+        return $this->vertices->sortBy(function ($vertex) {
+            return $vertex->degree();
+        })->first()->degree();
+    }
+
+    /**
+     * Degree: get maximum degree of vertices.
+     *
+     * @return int
+     */
+    public function getDegreeMax(): int
+    {
+        return $this->vertices->sortByDesc(function ($vertex) {
+            return $vertex->degree();
+        })->first()->degree();
     }
 
     /**
@@ -120,5 +161,150 @@ class Graph implements Attributable, Directable
         }
 
         return $new_graph;
+    }
+
+    /**
+     * Grouped: count total number of different groups assigned to vertices.
+     *
+     * @return int
+     */
+    public function getNumberOfGroups(): int
+    {
+        return count($this->getGroups());
+    }
+
+    /**
+     * Grouped: checks whether the input graph's vertex groups are a valid bipartition.
+     *
+     * @return bool
+     */
+    public function isBipartit(): bool
+    {
+        if ($this->getNumberOfGroups() !== 2) {
+            return false;
+        }
+
+        foreach ($this->vertices as $vertex) {
+            $group = $vertex->getAttribute('group');
+
+            foreach ($vertex->getVerticesTo() as $vertex_neighbor) {
+                if ($vertex_neighbor->getAttribute('group') === $group) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    /**
+     * Grouped: get vector of all group numbers.
+     *
+     * @return array
+     */
+    public function getGroups(): array
+    {
+        $groups = [];
+
+        foreach ($this->vertices as $vertex) {
+            $groups[$vertex->getAttribute('group')] = true;
+        }
+
+        return array_keys($groups);
+    }
+
+    /**
+     * Grouped: get set of all Vertices in the given group.
+     *
+     * @param int $group
+     *
+     * @return \PHGraph\Support\VertexCollection
+     */
+    public function getVerticesGroup(int $group): VertexCollection
+    {
+        return $this->vertices->filter(function ($vertex) use ($group) {
+            return $vertex->getAttribute('group') === $group;
+        });
+    }
+
+    /**
+     * Directed: checks whether the graph has any directed edges.
+     *
+     * @return bool
+     */
+    public function hasDirected(): bool
+    {
+        foreach ($this->getEdges() as $edge) {
+            if ($edge->isDirected()) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * Directed: checks whether the graph has any undirected edges.
+     *
+     * @return bool
+     */
+    public function hasUndirected(): bool
+    {
+        foreach ($this->getEdges() as $edge) {
+            if (!$edge->isDirected()) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * Directed: checks whether this is a mixed graph.
+     *
+     * @return bool
+     */
+    public function isMixed(): bool
+    {
+        return $this->hasDirected() && $this->hasUndirected();
+    }
+
+    /**
+     * Degree: checks whether the indegree of every vertex equals its outdegree.
+     *
+     * @return bool
+     */
+    public function isBalanced(): bool
+    {
+        foreach ($this->vertices as $vertex) {
+            if ($vertex->degreeIn() !== $vertex->degreeOut()) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    /**
+     * Degree: checks whether this graph is regular, i.e. each vertex has the
+     * same indegree/outdegree.
+     *
+     * @return bool
+     */
+    public function isRegular(): bool
+    {
+        if (count($this->vertices) === 0) {
+            return true;
+        }
+
+        try {
+            $this->getDegree();
+
+            return true;
+        } catch (UnexpectedValueException $ignore) {
+            // ignoring UnexpectedValueException
+        }
+
+        return false;
     }
 }
--- tests/GraphTest.php
+++ tests/GraphTest.php
@@ -7,6 +7,7 @@ use PHGraph\Support\EdgeCollection;
 use PHGraph\Support\VertexCollection;
 use PHGraph\Vertex;
 use PHPUnit\Framework\TestCase;
+use UnexpectedValueException;
 
 class GraphTest extends TestCase
 {
@@ -18,6 +19,69 @@ class GraphTest extends TestCase
     public function testInstantiation(): void
     {
         $this->assertInstanceOf(Graph::class, new Graph);
+    }
+
+    /**
+     * @covers PHGraph\Graph::getDegree
+     *
+     * @return void
+     */
+    public function testGetDegree(): void
+    {
+        $graph = new Graph;
+        $v1 = new Vertex($graph);
+        $v2 = new Vertex($graph);
+        $v1->createEdgeTo($v2);
+
+        $this->assertEquals(1, $graph->getDegree());
+    }
+
+    /**
+     * @covers PHGraph\Graph::getDegree
+     *
+     * @return void
+     */
+    public function testGetDegreeIrregular(): void
+    {
+        $this->expectException(UnexpectedValueException::class);
+
+        $graph = new Graph;
+        $v1 = new Vertex($graph);
+        $v2 = new Vertex($graph);
+        $v1->createEdgeTo($v2);
+        $v1->createEdge($v1);
+
+        $graph->getDegree();
+    }
+
+    /**
+     * @covers PHGraph\Graph::getDegreeMin
+     *
+     * @return void
+     */
+    public function testGetDegreeMin(): void
+    {
+        $graph = new Graph;
+        $v1 = new Vertex($graph);
+        $v2 = new Vertex($graph);
+        $v1->createEdge($v2);
+
+        $this->assertEquals(1, $graph->getDegreeMin());
+    }
+
+    /**
+     * @covers PHGraph\Graph::getDegreeMax
+     *
+     * @return void
+     */
+    public function testGetDegreeMax(): void
+    {
+        $graph = new Graph;
+        $v1 = new Vertex($graph);
+        $v2 = new Vertex($graph);
+        $v1->createEdgeTo($v2);
+
+        $this->assertEquals(1, $graph->getDegreeMax());
     }
 
     /**
@@ -149,5 +213,328 @@ class GraphTest extends TestCase
         $edge = $vertex_a->createEdge($vertex_b);
 
         $this->assertNotSame($edge, array_values($graph->newFromEdges(new EdgeCollection([$edge]))->getEdges()->all())[0]);
+    }
+
+    /**
+     * @covers PHGraph\Graph::getNumberOfGroups
+     *
+     * @return void
+     */
+    public function testGetNumberOfGroupsEmpty(): void
+    {
+        $graph = new Graph;
+
+        $this->assertEquals(0, $graph->getNumberOfGroups());
+    }
+
+    /**
+     * @covers PHGraph\Graph::getNumberOfGroups
+     *
+     * @return void
+     */
+    public function testGetNumberOfGroups(): void
+    {
+        $graph = new Graph;
+        $v1 = new Vertex($graph);
+        $v2 = new Vertex($graph);
+        $v1->setAttribute('group', 0);
+        $v2->setAttribute('group', 1);
+
+        $this->assertEquals(2, $graph->getNumberOfGroups());
+    }
+
+    /**
+     * @covers PHGraph\Graph::isBipartit
+     *
+     * @return void
+     */
+    public function testIsBipartitTrue(): void
+    {
+        $graph = new Graph;
+        $v1 = new Vertex($graph);
+        $v2 = new Vertex($graph);
+        $v3 = new Vertex($graph);
+        $v4 = new Vertex($graph);
+        $v1->setAttribute('group', 0);
+        $v3->setAttribute('group', 0);
+        $v2->setAttribute('group', 1);
+        $v4->setAttribute('group', 1);
+        $v1->createEdge($v2);
+        $v1->createEdge($v4);
+        $v2->createEdge($v3);
+
+        $this->assertTrue($graph->isBipartit());
+    }
+
+    /**
+     * @covers PHGraph\Graph::isBipartit
+     *
+     * @return void
+     */
+    public function testIsBipartitFalse(): void
+    {
+        $graph = new Graph;
+        $v1 = new Vertex($graph);
+        $v2 = new Vertex($graph);
+        $v3 = new Vertex($graph);
+        $v4 = new Vertex($graph);
+        $v1->setAttribute('group', 0);
+        $v3->setAttribute('group', 1);
+        $v2->setAttribute('group', 1);
+        $v4->setAttribute('group', 1);
+        $v1->createEdge($v2);
+        $v1->createEdge($v4);
+        $v2->createEdge($v3);
+
+        $this->assertFalse($graph->isBipartit());
+    }
+
+    /**
+     * @covers PHGraph\Graph::isBipartit
+     *
+     * @return void
+     */
+    public function testIsBipartitFalseMoreThanTwoGroups(): void
+    {
+        $graph = new Graph;
+        $v1 = new Vertex($graph);
+        $v2 = new Vertex($graph);
+        $v3 = new Vertex($graph);
+        $v4 = new Vertex($graph);
+        $v1->setAttribute('group', 0);
+        $v3->setAttribute('group', 0);
+        $v2->setAttribute('group', 1);
+        $v4->setAttribute('group', 3);
+        $v1->createEdge($v2);
+        $v1->createEdge($v4);
+        $v2->createEdge($v3);
+
+        $this->assertFalse($graph->isBipartit());
+    }
+
+    /**
+     * @covers PHGraph\Graph::getGroups
+     *
+     * @return void
+     */
+    public function testGetGroups(): void
+    {
+        $graph = new Graph;
+        $v1 = new Vertex($graph);
+        $v2 = new Vertex($graph);
+        $v3 = new Vertex($graph);
+        $v4 = new Vertex($graph);
+        $v1->setAttribute('group', 0);
+        $v3->setAttribute('group', 0);
+        $v2->setAttribute('group', 1);
+        $v4->setAttribute('group', 3);
+        $v1->createEdge($v2);
+        $v1->createEdge($v4);
+        $v2->createEdge($v3);
+
+        $this->assertEquals([0, 1, 3], $graph->getGroups());
+    }
+
+    /**
+     * @covers PHGraph\Graph::getVerticesGroup
+     *
+     * @return void
+     */
+    public function testGetVerticesGroup(): void
+    {
+        $graph = new Graph;
+        $v1 = new Vertex($graph);
+        $v2 = new Vertex($graph);
+        $v3 = new Vertex($graph);
+        $v4 = new Vertex($graph);
+        $v1->setAttribute('group', 0);
+        $v3->setAttribute('group', 0);
+        $v2->setAttribute('group', 1);
+        $v4->setAttribute('group', 3);
+        $v1->createEdge($v2);
+        $v1->createEdge($v4);
+        $v2->createEdge($v3);
+
+        $this->assertEquals([$v1, $v3], $graph->getVerticesGroup(0)->all());
+    }
+
+    /**
+     * @covers PHGraph\Graph::hasDirected
+     *
+     * @return void
+     */
+    public function testHasDirectedTrue(): void
+    {
+        $graph = new Graph;
+        $v1 = new Vertex($graph);
+        $v2 = new Vertex($graph);
+        $edge = $v1->createEdgeTo($v2);
+
+        $this->assertTrue($graph->hasDirected());
+    }
+
+    /**
+     * @covers PHGraph\Graph::hasDirected
+     *
+     * @return void
+     */
+    public function testHasDirectedFalse(): void
+    {
+        $graph = new Graph;
+        $v1 = new Vertex($graph);
+        $v2 = new Vertex($graph);
+        $edge = $v1->createEdge($v2);
+
+        $this->assertFalse($graph->hasDirected());
+    }
+
+    /**
+     * @covers PHGraph\Graph::hasUndirected
+     *
+     * @return void
+     */
+    public function testHasUndirectedTrue(): void
+    {
+        $graph = new Graph;
+        $v1 = new Vertex($graph);
+        $v2 = new Vertex($graph);
+        $edge = $v1->createEdge($v2);
+
+        $this->assertTrue($graph->hasUndirected());
+    }
+
+    /**
+     * @covers PHGraph\Graph::hasUndirected
+     *
+     * @return void
+     */
+    public function testHasUndirectedFalse(): void
+    {
+        $graph = new Graph;
+        $v1 = new Vertex($graph);
+        $v2 = new Vertex($graph);
+        $edge = $v1->createEdgeTo($v2);
+
+        $this->assertFalse($graph->hasUndirected());
+    }
+
+    /**
+     * @covers PHGraph\Graph::isMixed
+     *
+     * @return void
+     */
+    public function testIsMixedTrue(): void
+    {
+        $graph = new Graph;
+        $v1 = new Vertex($graph);
+        $v2 = new Vertex($graph);
+        $edge1 = $v1->createEdgeTo($v2);
+        $edge2 = $v1->createEdge($v2);
+
+        $this->assertTrue($graph->isMixed());
+    }
+
+    /**
+     * @covers PHGraph\Graph::isMixed
+     *
+     * @return void
+     */
+    public function testIsMixedFalseDirected(): void
+    {
+        $graph = new Graph;
+        $v1 = new Vertex($graph);
+        $v2 = new Vertex($graph);
+        $edge = $v1->createEdgeTo($v2);
+
+        $this->assertFalse($graph->isMixed());
+    }
+
+    /**
+     * @covers PHGraph\Graph::isMixed
+     *
+     * @return void
+     */
+    public function testIsMixedFalseUndirected(): void
+    {
+        $graph = new Graph;
+        $v1 = new Vertex($graph);
+        $v2 = new Vertex($graph);
+        $edge = $v1->createEdge($v2);
+
+        $this->assertFalse($graph->isMixed());
+    }
+
+    /**
+     * @covers PHGraph\Graph::isBalanced
+     *
+     * @return void
+     */
+    public function testIsBalancedTrue(): void
+    {
+        $graph = new Graph;
+        $v1 = new Vertex($graph);
+        $v2 = new Vertex($graph);
+        $v1->createEdge($v2);
+
+        $this->assertTrue($graph->isBalanced());
+    }
+
+    /**
+     * @covers PHGraph\Graph::isBalanced
+     *
+     * @return void
+     */
+    public function testIsBalancedFalse(): void
+    {
+        $graph = new Graph;
+        $v1 = new Vertex($graph);
+        $v2 = new Vertex($graph);
+        $v1->createEdgeTo($v2);
+
+        $this->assertFalse($graph->isBalanced());
+    }
+
+    /**
+     * @covers PHGraph\Graph::isRegular
+     *
+     * @return void
+     */
+    public function testIsRegularEmptyTrue(): void
+    {
+        $graph = new Graph;
+
+        $this->assertTrue($graph->isRegular());
+    }
+
+    /**
+     * @covers PHGraph\Graph::isRegular
+     *
+     * @return void
+     */
+    public function testIsRegularTrue(): void
+    {
+        $graph = new Graph;
+        $v1 = new Vertex($graph);
+        $v2 = new Vertex($graph);
+        $v1->createEdgeTo($v2);
+        $v1->createEdge($v2);
+
+        $this->assertTrue($graph->isRegular());
+    }
+
+    /**
+     * @covers PHGraph\Graph::isRegular
+     *
+     * @return void
+     */
+    public function testIsRegularFalse(): void
+    {
+        $graph = new Graph;
+        $v1 = new Vertex($graph);
+        $v2 = new Vertex($graph);
+        $v1->createEdgeTo($v2);
+        $v1->createEdgeTo($v1);
+
+        $this->assertFalse($graph->isRegular());
     }
 }
